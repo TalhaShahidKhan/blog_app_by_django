3b71ea5fce209363cd502c8fa835c9ff5bb63834
talhakhan54 <talhashahidkhan54@gmail.com>
2023-02-22 11:09:26 +0600

    --

--- CodeBlog/urls.py
+++ CodeBlog/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from home.views import landingpage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',landingpage,name="LP"),
+    path('home/', include('home.urls')),
+    path('blog/', include('blog.urls')),
 ]
